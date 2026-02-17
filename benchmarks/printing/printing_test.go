33a9a00341141093f1de6613eacf47dea12e39a1
MarvinJWendt <git@marvinjwendt.com>
2026-02-17 02:31:12 +0100

    feat: update benchmarks for fmt printing functions

--- benchmarks/printing/printing_test.go
+++ benchmarks/printing/printing_test.go
@@ -1,22 +1,16 @@
-package fmt
+package printing
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"testing"
 )
 
 const s = "Hello, World!"
 
-func BenchmarkFmtPrintln_run(b *testing.B) {
-	os.Stdout, _ = os.Open(os.DevNull)
-
-	for i := 0; i < b.N; i++ {
-		fmt.Println(s)
-	}
-}
-
-func BenchmarkFmtPrint_run(b *testing.B) {
+// Print writes to stdout.
+func BenchmarkPrint_run(b *testing.B) {
 	os.Stdout, _ = os.Open(os.DevNull)
 
 	for i := 0; i < b.N; i++ {
@@ -24,10 +18,41 @@ func BenchmarkFmtPrint_run(b *testing.B) {
 	}
 }
 
-func BenchmarkFmtPrintf_run(b *testing.B) {
+// Println writes to stdout with a trailing newline.
+func BenchmarkPrintln_run(b *testing.B) {
+	os.Stdout, _ = os.Open(os.DevNull)
+
+	for i := 0; i < b.N; i++ {
+		fmt.Println(s)
+	}
+}
+
+// Printf writes a formatted string to stdout.
+func BenchmarkPrintf_run(b *testing.B) {
 	os.Stdout, _ = os.Open(os.DevNull)
 
 	for i := 0; i < b.N; i++ {
 		fmt.Printf("%s\n", s)
+	}
+}
+
+// Fprint writes to an io.Writer.
+func BenchmarkFprint_run(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Fprint(io.Discard, s)
+	}
+}
+
+// Fprintln writes to an io.Writer with a trailing newline.
+func BenchmarkFprintln_run(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Fprintln(io.Discard, s)
+	}
+}
+
+// Fprintf writes a formatted string to an io.Writer.
+func BenchmarkFprintf_run(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Fprintf(io.Discard, "%s\n", s)
 	}
 }
